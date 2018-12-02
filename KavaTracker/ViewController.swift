da5731560cc318ae0a6ba0327203a51a38a459e4
brandonberger <bdonberger@gmail.com>
2018-12-02 13:42:54 -0500

    First

--- KavaTracker/ViewController.swift
+++ KavaTracker/ViewController.swift
@@ -9,11 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let lastKnownKratomCount = UserDefaults.standard.value(forKey: "currentKratomCount") as? Int
+    let lastKnownKratomCountString = UserDefaults.standard.value(forKey: "currentKratomCount") as? String
+    
+    var kratomCount:Int = 0
+    
+    func checkForCurrentKratomCount() {
+        if ((lastKnownKratomCount) != nil) {
+            kratomCount = lastKnownKratomCount!
+            kratomCounter.text = String(kratomCount)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        checkForCurrentKratomCount()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBOutlet weak var kratomCounter: UILabel!
+
+    @IBAction func addKratom(_ sender: UIButton) {
+        kratomCount = kratomCount + 1
+        kratomCounter.text = String(kratomCount)
+        UserDefaults.standard.set(kratomCount, forKey: "currentKratomCount")
+    }
+    
+    @IBAction func subtractKratom(_ sender: UIButton) {
+        kratomCount = kratomCount - 1
+        kratomCounter.text = String(kratomCount)
+        UserDefaults.standard.set(kratomCount, forKey: "currentKratomCount")
+    }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
